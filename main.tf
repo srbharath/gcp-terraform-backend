8d59289e1c010e1c1a99511c3a2effc5169aeb82
srbharath <srbharath41@gmail.com>
2023-08-02 11:53:15 +0530

    second commit

--- main.tf
+++ main.tf
@@ -25,10 +25,10 @@ resource "google_storage_bucket" "GCS1" {
   }
 }
 
-terraform {
-  backend "gcs" {
-    bucket = "2-storage-buckect"
-    prefix = "sftp-to-bucket/terraform.tfstate"
-    # credentials = "./keys.json"
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "2-storage-buckect"
+#     prefix = "sftp-to-bucket/terraform.tfstate"
+#     # credentials = "./keys.json"
+#   }
+# }
